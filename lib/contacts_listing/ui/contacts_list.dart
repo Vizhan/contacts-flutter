c67065062ce7220d86e78a7a51bc8f0bf6f46366
Oleksii Vizhan <vizhan@lanars.com>
2020-10-28 09:01:00 +0200

    Refactor contacts listing

--- lib/contacts_listing/ui/contacts_list.dart
+++ lib/contacts_listing/ui/contacts_list.dart
@@ -15,36 +15,64 @@ class ContactList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (!contactsViewModel.isUserPermissionGranted) {
-      return Center(
-        child: Container(
-          child: Text('User permission required'),
-        ),
-      );
+      return _buildPermissionMessage();
     }
 
     if (contactsViewModel.contacts.isNotEmpty) {
-      return Scrollbar(
-        radius: Radius.circular(16),
-        child: ListView.separated(
-          controller: scrollController,
-          itemCount: contactsViewModel.contacts.length,
-          padding: EdgeInsets.all(16),
-          separatorBuilder: (context, index) => const SizedBox(height: 8),
-          itemBuilder: (context, index) {
-            return ContactItem(
-              contact: contactsViewModel.contacts[index],
-            );
-          },
-        ),
-      );
+      return _buildContacts();
     }
 
     if (contactsViewModel.isLoading) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
+      return _buildLoadingIndicator();
     }
 
+    return _buildEmptyState();
+  }
+
+  Padding _buildPermissionMessage() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'User permission required.',
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Please, allow access to contacts in the application settings.',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Scrollbar _buildContacts() {
+    return Scrollbar(
+      radius: Radius.circular(16),
+      child: ListView.separated(
+        controller: scrollController,
+        itemCount: contactsViewModel.contacts.length,
+        padding: EdgeInsets.all(16),
+        separatorBuilder: (context, index) => const SizedBox(height: 8),
+        itemBuilder: (context, index) {
+          return ContactItem(
+            contact: contactsViewModel.contacts[index],
+          );
+        },
+      ),
+    );
+  }
+
+  Center _buildLoadingIndicator() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Center _buildEmptyState() {
     return Center(
       child: Text('No contacts'),
     );
