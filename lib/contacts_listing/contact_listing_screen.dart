ee39b483a2a72a34af0084840a1229815f7236d8
Oleksii Vizhan <vizhan@lanars.com>
2020-10-28 02:52:02 +0200

    Update screen mark up

--- lib/contacts_listing/contact_listing_screen.dart
+++ lib/contacts_listing/contact_listing_screen.dart
@@ -8,6 +8,8 @@ import 'package:rebloc/rebloc.dart';
 
 import 'contact_listing_actions.dart';
 
+const List<String> alphabetData = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "X", "Y", "Z"];
+
 class ContactsScreen extends StatefulWidget {
   @override
   _ContactsScreenState createState() => _ContactsScreenState();
@@ -28,10 +30,14 @@ class _ContactsScreenState extends State<ContactsScreen> {
     super.initState();
     //TODO look for better solution
     Future.delayed(Duration.zero, () {
-      StoreProvider.of<AppState>(context).states.stream.listen((event) {
-        final invalidIndex = -1;
+      StoreProvider
+          .of<AppState>(context)
+          .states
+          .stream
+          .listen((event) {
+        final noScrollNeeded = -1;
         final destinationIndex = event.contactListingState.jumpTo;
-        if (destinationIndex != invalidIndex && contactsScrollController.hasClients) {
+        if (destinationIndex != noScrollNeeded && contactsScrollController.hasClients) {
           contactsScrollController?.animateTo(_persistentItemHeight * destinationIndex.toDouble(),
               duration: Duration(seconds: 1), curve: Curves.linear);
         }
@@ -42,7 +48,7 @@ class _ContactsScreenState extends State<ContactsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _display('Contacts'),
+      appBar: _display("Contacts"),
       body: _buildScreenBody(context),
       bottomSheet: _buildPersistentBottomSheet(context),
     );
@@ -58,6 +64,7 @@ class _ContactsScreenState extends State<ContactsScreen> {
   SafeArea _buildScreenBody(BuildContext context) {
     return SafeArea(
       child: Padding(
+
         /// https://github.com/flutter/flutter/issues/50314
         padding: EdgeInsets.only(bottom: _bottomSheetHeight),
         child: Column(
@@ -77,27 +84,63 @@ class _ContactsScreenState extends State<ContactsScreen> {
         right: 16,
         bottom: 16,
       ),
-      child: SearchBar(
-        controller: searchController,
-        focusNode: searchFocusNode,
-        hintText: 'Search people',
-        onSearchPressedCallback: (query) {
-          StoreProvider.of<AppState>(context).dispatch(SearchContactAction(query: query));
-        },
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: SearchBar(
+              controller: searchController,
+              focusNode: searchFocusNode,
+              hintText: "Search people",
+              onSearchPressedCallback: (query) {
+                StoreProvider.of<AppState>(context).dispatch(SearchContactAction(query: query));
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(
+                Icons.add,
+                color: Colors.indigo,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Expanded _buildContactList() {
     return Expanded(
-      child: ViewModelSubscriber<AppState, ContactListingState>(
-        converter: (state) => state.contactListingState,
-        builder: (context, dispatcher, contactsViewModel) {
-          return ContactList(
-            contactsViewModel,
-            contactsScrollController,
-          );
-        },
+      child: Row(
+        children: [
+          Expanded(
+            child: ViewModelSubscriber<AppState, ContactListingState>(
+              converter: (state) => state.contactListingState,
+              builder: (context, dispatcher, contactsViewModel) {
+                return ContactList(
+                  contactsViewModel,
+                  contactsScrollController,
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Scrollbar(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: alphabetData.map((e) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                    child: Text(e),
+                  )).toList(),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
